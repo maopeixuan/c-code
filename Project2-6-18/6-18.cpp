01924a06264cebc21c1a4b9faaa6f53a595576a7
maopeixuan <maopeixuann@163.com>
2022-06-29 15:22:07 +0800

    code

---
+++ Project2-6-18/6-18.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+enum sex
+{
+     male,
+	 female,
+	 unknown
+};
+int main()
+{
+#define jj 12
+	/*const int shu = 4;
+	printf("%d\n", shu);
+	int shu = 5;*/
+	const int n = 8;
+	int arr[n] = { 0 };
+	printf("%d\n", male);
+	return 0; 
+	{
+		enum sex xingbie = male;
+
+		return 0;
+	}
+}
+
